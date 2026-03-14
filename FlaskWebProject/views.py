a57dbbf14a8bc6c8bdcfee82200b15bf466aa497
Vandana-Dedeepya <vandanadedeepya1601@gmail.com>
2026-03-15 01:00:54 +0530

    Added funny login logging

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -12,6 +12,7 @@ from flask_login import current_user, login_user, logout_user, login_required
 from FlaskWebProject.models import User, Post
 import msal
 import uuid
+import logging
 
 imageSourceUrl = 'https://images47.blob.core.windows.net/' + app.config['BLOB_CONTAINER'] + '/'
 
@@ -42,7 +43,6 @@ def new_post():
         form=form
     )
 
-
 @app.route('/post/<int:id>', methods=['GET', 'POST'])
 @login_required
 def post(id):
@@ -62,67 +62,86 @@ def post(id):
 def login():
     if current_user.is_authenticated:
         return redirect(url_for('home'))
+
     form = LoginForm()
+
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
+
+        # Invalid login attempt logging
         if user is None or not user.check_password(form.password.data):
+            app.logger.warning("🚨 ALERT! Someone tried to break in with the wrong password! Nice try hacker 😎")
             flash('Invalid username or password')
             return redirect(url_for('login'))
+
+        # Successful login logging
         login_user(user, remember=form.remember_me.data)
+        app.logger.info("🎉 SUCCESS! The admin has entered the building")
+
         next_page = request.args.get('next')
         if not next_page or urlparse(next_page).netloc != '':
             next_page = url_for('home')
+
         return redirect(next_page)
+
     session["state"] = str(uuid.uuid4())
     auth_url = _build_auth_url(scopes=Config.SCOPE, state=session["state"])
-    return render_template('login.html', title='Sign In', form=form, auth_url=auth_url)
 
-@app.route(Config.REDIRECT_PATH)  # Its absolute URL must match your app's redirect_uri set in AAD
+    return render_template(
+        'login.html',
+        title='Sign In',
+        form=form,
+        auth_url=auth_url
+    )
+
+@app.route(Config.REDIRECT_PATH)
 def authorized():
     if request.args.get('state') != session.get("state"):
-        return redirect(url_for("home"))  # No-OP. Goes back to Index page
-    if "error" in request.args:  # Authentication/Authorization failure
+        return redirect(url_for("home"))
+
+    if "error" in request.args:
         return render_template("auth_error.html", result=request.args)
+
     if request.args.get('code'):
         cache = _load_cache()
-        # TODO: Acquire a token from a built msal app, along with the appropriate redirect URI
+
         result = None
+
         if "error" in result:
             return render_template("auth_error.html", result=result)
+
         session["user"] = result.get("id_token_claims")
-        # Note: In a real app, we'd use the 'name' property from session["user"] below
-        # Here, we'll use the admin username for anyone who is authenticated by MS
+
         user = User.query.filter_by(username="admin").first()
         login_user(user)
+
         _save_cache(cache)
+
     return redirect(url_for('home'))
 
 @app.route('/logout')
 def logout():
     logout_user()
-    if session.get("user"): # Used MS Login
-        # Wipe out user and its token cache from session
+
+    if session.get("user"):
         session.clear()
-        # Also logout from your tenant's web session
+
         return redirect(
             Config.AUTHORITY + "/oauth2/v2.0/logout" +
-            "?post_logout_redirect_uri=" + url_for("login", _external=True))
+            "?post_logout_redirect_uri=" + url_for("login", _external=True)
+        )
 
     return redirect(url_for('login'))
 
 def _load_cache():
-    # TODO: Load the cache from `msal`, if it exists
     cache = None
     return cache
 
 def _save_cache(cache):
-    # TODO: Save the cache, if it has changed
     pass
 
 def _build_msal_app(cache=None, authority=None):
-    # TODO: Return a ConfidentialClientApplication
     return None
 
 def _build_auth_url(authority=None, scopes=None, state=None):
-    # TODO: Return the full Auth Request URL with appropriate Redirect URI
     return None
